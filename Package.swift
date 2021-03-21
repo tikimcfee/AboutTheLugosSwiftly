0afd19e5f75b95bdea9b05c27733fe65d37becfc
Ivan Lugo <ivan.lugo@knights.ucf.edu>
2021-03-20 21:14:56 -0700

    moar

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3.0
 
 import PackageDescription
 
 let package = Package(
     name: "VaporLugos",
     platforms: [
-//       .macOS(.v10_15)
+       .macOS(.v10_15),
     ],
     products: [
          .library(
