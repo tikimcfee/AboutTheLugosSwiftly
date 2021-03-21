02781e7568b5745d1d158a41c5abeb8ae3ec3fcb
Ivan Lugo <ivan.lugo@knights.ucf.edu>
2021-03-20 21:06:28 -0700

    maybe? just stuff..

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "VaporLugos",
     platforms: [
-       .macOS(.v10_15)
+//       .macOS(.v10_15)
     ],
     products: [
          .library(
