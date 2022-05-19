d609c83dfb3499e9cb8c3f942c750b7a4361041e
Ivan Lugo <ivan.lugo@knights.ucf.edu>
2022-05-18 23:33:13 -0700

    - Tweak release

--- release.sh
+++ release.sh
@@ -1,2 +1,8 @@
-swift build -c release --enable-test-discovery &&
-cp "/Users/ivan.lugo/personal_projects/VaporLugos/.build/release/Run" .
+swift build -c release &&
+cp ".build/release/Run" .
+
+# rm -rf "release-package"
+# mkdir "release-package"
+# cp ".build/release/Run" "release-package/"
+# cp "start-server.sh" "release-package/"
+# cp -r "Public" "release-package/"
